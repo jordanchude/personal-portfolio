bb5e6a39160989f42fa70e212ee07fe1e1c951c4
JordanC <jcwilliams410@gmail.com>
2020-09-29 15:53:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//google sheets public url
+//https://docs.google.com/spreadsheets/d/11lnTz3OBSIe-60N-j3JCw09E-zJCTuMizcRa6NaJXVs/edit#gid=0
+
+//json template
+// https://spreadsheets.google.com/feeds/list/    /od6/public/values?alt=json 
+
+//customized json
+// https://spreadsheets.google.com/feeds/list/11lnTz3OBSIe-60N-j3JCw09E-zJCTuMizcRa6NaJXVs/od6/public/values?alt=json 
